d73ab3adb7dd69854e26bba8eae262f9ef361bc3
priyaranjan rai <priyranjanrai@gmail.com>
2017-07-01 00:45:19 +0530

    Add files via upload

--- chain.m
+++ chain.m
@@ -1,35 +1,37 @@
 clc;
 clear;
-a=imread('C:\Users\Shant-Gamma\Desktop\81.jpg');
+a=imread('C:\Users\Shant-Gamma\Desktop\image\5.jpg');
 b=rgb2gray(a);
-c=b<128;
+c=b<180;
 e=imresize(c,[64 64]);
 d=bwmorph(e,'thin',Inf);
-sx=-1;
-sy=-1;
+st=zeros(2,10);
 tr=zeros(64);
 ch=zeros(1,4000);
-flag1=0;
+m=1;
+horizontal=0;
+vertical=0;
+ridia=0;
+ledia=0;
+flag8=0;
 for i=2:63
     for j=2:63
         if(d(i,j)==1 && neighbour(d,i,j)==1)
-            sx=i;
-            sy=j;
-            flag1=1;
-            break
+            st(1,m)=i;
+            st(2,m)=j;
+            m=m+1;
         end
     end
-    if(flag1==1)
-        break
-    end
-    end
+end
 flag1=0;
-if(sx==-1)
+if(m==1)
+    flag8=1;
     for i=1:64
         for j=1:64
             if(d(i,j)==1)
-            sx=i;
-            sy=j;
+            st(1,m)=i;
+            st(2,m)=j;
+            m=m+1;
             flag1=1;
             break
             end
@@ -39,15 +41,34 @@ if(sx==-1)
         end
     end
 end
-%jun=zeros(64,64);
-%for i=1:64
-  %  for j=1:64
-        %if(neighbour(d,i,j)>2)
-           % jun(i,j)=1;
-        %end
-   % end
+l=m-1;
+junc=zeros(64);
+m=0;
+canc=zeros(64);
+for i=2:63
+    for j=2:63
+        if(d(i,j)==1 && trueneighbours(d,i,j)>=3 && canc(i,j)==0)
+            %jun(i,j)=1;
+            m=m+1;
+            junc(i,j)=1;
+            %junc(3,m)=trueneighbours(d,i,j);
+        end
+    end
+end
+%noOfJunction=m
+%for i=1:m
+    %x=junc(1,i)
+    %y=junc(2,i)
+    %neighbour(d,junc(1,i),junc(2,i))
 %end
+%[area,perimeter]=regarea(d)
+for i=1:l
 m=1;
+sx=st(1,i);
+sy=st(2,i);
+if(tr(sx,sy)==1)
+    break;
+end
 tr(sx,sy)=1;
 if(d(sx,sy+1)==1)
     ch(m)=0;
@@ -129,10 +150,15 @@ if(pe==9)
 end
 if(dir(sa)==1)
     ch(m)=sa-1;
-elseif(dir(pe)==1)
-    ch(m)=pe-1;
-elseif(dir(ag)==1)
-    ch(m)=ag-1;
+%elseif(dir(pe)==1)
+    %ch(m)=pe-1;
+%elseif(dir(ag)==1)
+    %ch(m)=ag-1;
+end
+if(ch(m)==0)
+    if(junc(r,c)==1 && flag8==0)
+        break;
+    end
 else
     for ct=1:8
         if(dir(ct)==1 && ct~=mod((3+sa),8)+1)
@@ -175,8 +201,26 @@ end
     end
 end
 m=m-flag;
-disp(sx)
-disp(sy)
+startx=sx
+starty=sy
 for i=1:m
     disp(ch(i))
 end
+if(m>2)
+[h,r,v,le]=lines(ch,m);
+horizontal=horizontal+h;
+ridia=ridia+r;
+vertical=vertical+v;
+ledia=ledia+le;
+end
+end
+horizontal
+normalHor=1-((horizontal/10)*2)
+vertical
+normalVert=1-((vertical/10)*2)
+ridia
+normalrdia=1-((ridia/10)*2)
+ledia
+normaldia=1-((ledia/10)*2)
+[area,perimeter]=regarea(d)
+loops=1-bweuler(d,8)
